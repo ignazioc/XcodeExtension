990dc3884303032ab99f8f8b03e1fb492d99cc2f
ignazioc <ignazioc@gmail.com>
2017-08-17 06:17:44 +0200

    Initial Commit

---
+++ XcodeExtension/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  XcodeExtension
+//
+//  Created by Ignazio Calò on 17/08/2017.
+//  Copyright © 2017 Ignazio Calò. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ XcodeExtension/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  XcodeExtension
+//
+//  Created by Ignazio Calò on 17/08/2017.
+//  Copyright © 2017 Ignazio Calò. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
